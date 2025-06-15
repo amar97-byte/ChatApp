d6d2f06b7339172d2382a2166bcf70c3fd7f05ae
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-16 02:08:13 +0530

    Sidebar working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Profile from "./pages/Profile"
+// import {assets} from "./assets/assets.js"
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain " >
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
     </div>
   )
 }
