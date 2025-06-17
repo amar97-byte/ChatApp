0dd05cd705bc713d28ce1e65ee79ab262680f9d1
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-18 01:48:25 +0530

    Authantication function

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,24 @@
+import { JsonWebTokenError } from "jsonwebtoken";
+import User from "../models/user.model.js";
+
+// ----- Middleware to protect routes-----
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const token = req.headers.token;
+
+    const decoded = Jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = new User.findById(decoded.userId).select("-password");
+
+    if (!user) {
+      return res.json({ success: false, message: "user not found" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    return res.json({ success: false, message: error.message});
+  }
+};
