098ad426169d1ae47212cb4758dec4d6c12572e9
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-18 00:46:37 +0530

    Backend started

---
+++ server/server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from "./lib/db.js";
+
+const app = express();
+
+// ---- Socket.io support the http server---
+const server = http.createServer(app);
+
+// Middleware
+app.use(express.json({ limit: "4mb" }));
+app.use(cors());
+
+app.use("/api/status", (req, res) => {
+  res.send("Server is live");
+});
+
+//----- Connect to mongodb----
+await connectDB()
+
+
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log("Server is running on PORT : " + PORT));
+
+
