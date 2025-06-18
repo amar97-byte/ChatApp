0307829e4c34e6e6a88bd00aebea9f5172c1e15a
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-19 01:57:03 +0530

    message routes are made

--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/message.contoller.js"
+import { getMessages, getUsersForSidebar, markMessageAsSeen } from "../controllers/message.controller.js"
 
 const messageRouter = express.Router()
 
