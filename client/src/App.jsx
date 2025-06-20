581f6c7c07adb57d536ba549bd2a97d71f53d172
Amarjeet <amarjeetgiri020@gmail.com>
2025-06-20 23:37:44 +0530

    authenticated routes for pages are formed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,23 @@
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
 import {Toaster} from "react-hot-toast"
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
 // import {assets} from "./assets/assets.js"
 
 const App = () => {
+
+  const {authUser} = useContext(AuthContext)
+
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain " >
-      <Routes>
         <Toaster/>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/profile" element={<Profile/>}/>
+      <Routes>
+        <Route path="/" element={authUser ?  <Home/> : <Navigate to="/login" />}/>
+        <Route path="/login" element={!authUser ? <Login/> : <Navigate to="/" />}/>
+        <Route path="/profile" element={authUser ? <Profile/> : <Navigate to="login" />}/>
       </Routes>
     </div>
   )
